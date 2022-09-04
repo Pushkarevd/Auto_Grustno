d5fba63f201ac621dc6c35ec03e424920a018a64
Pushkarev Daniil <42956510+Pushkarevd@users.noreply.github.com>
2022-09-04 19:32:13 +0300

    Update main.py
    Add prints in cycle

--- main.py
+++ main.py
@@ -47,10 +47,12 @@ def main():
         for post in posts:
             post_id = post.get('id')
             instance.like(post_id)
+            print(post_id)
         posts = instance.get_hot_posts()
         for post in posts:
             post_id = post.get('id')
             instance.like(post_id)
+            print(post_id)
         sleep(10)
         counter += 10
         if counter == 10800:
