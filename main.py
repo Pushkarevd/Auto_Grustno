708bc5c4fe08372a81f1743be1d0d62ebff63bf4
PC <pushkarevd.89@gmail.com>
2022-09-10 22:54:51 +0300

    Change logic of main loop

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from subs_module.load_subs import update_subs
 params = {
     'database': 'grustno_bot',
     'user': 'grustno_bot',
-    'password': environ.get('db_pass'),
-    'host': environ.get('db_host', '127.0.0.1'),
+    'password': 'no1trol',
+    'host': '192.168.0.137',
     'port': '5432'
 }
 
@@ -30,7 +30,7 @@ def like_all_posts(instance: gr_api.Grustno, diff_set_hot: list, diff_set_unknow
                     connector.execute_command(command)
 
                 diff_set.append(post_id)
-                instance.like(post_id)
+                print(instance.like(post_id))
                 sleep(.2)
                 instance.like(post_id)
         for post in posts:
@@ -54,13 +54,17 @@ def check_diff_sets(diff_set_hot: list, diff_set_unknown: list) -> tuple[list, l
 
 if __name__ == "__main__":
     instance = gr_api.Grustno()
-    counter = 0
     diff_set_unknown = []
     diff_set_hot = []
     connector = Connector(**params)
+    counter = 0
     while True:
         like_all_posts(instance, diff_set_hot, diff_set_unknown, connector)
         sleep(10)
+        counter += 1
         diff_set_unknown, diff_set_hot = check_diff_sets(diff_set_hot, diff_set_unknown)
-        update_subs(instance, connector)
+        if counter == 6:
+            counter = 0
+            update_subs(instance, connector)
+        break
 
